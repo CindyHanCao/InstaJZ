5caf1d6bd675b2e5db706a37f9c6f6821d86466e
naonaomacindy <chcaohan@gmail.com>
2019-08-04 12:22:05 -0700

    Authentication and Authorization Worked

--- Insta/urls.py
+++ Insta/urls.py
@@ -21,9 +21,9 @@ from Insta.views import (HelloWorld, PostsView, PostDetailView, PostCreateView,
 
 urlpatterns = [
     path('', HelloWorld.as_view(), name='helloworld'),
-    path('posts/', PostsView.as_view(), name = 'index'),
-    path('posts/<int:pk>', PostDetailView.as_view(), name = 'post_detail'),
-    path('posts/new', PostCreateView.as_view(), name = 'make_post'),
-    path('posts/update/<int:pk>', PostUpdateView.as_view(), name = 'post_update'),
-    path('posts/delete/<int:pk>', PostDeleteView.as_view(), name = 'post_delete'),
+    path('posts', PostsView.as_view(), name = 'posts'),
+    path('post/<int:pk>', PostDetailView.as_view(), name = 'post_detail'),
+    path('post/new', PostCreateView.as_view(), name = 'make_post'),
+    path('post/update/<int:pk>', PostUpdateView.as_view(), name = 'post_update'),
+    path('post/delete/<int:pk>', PostDeleteView.as_view(), name = 'post_delete'),
 ]
--- Insta/views.py
+++ Insta/views.py
@@ -4,6 +4,10 @@ from django.urls import reverse_lazy
 
 from Insta.models import Post
 
+from Insta.forms import CustomUserCreationForm
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 class HelloWorld(TemplateView):
     template_name = 'test.html'
 
@@ -15,10 +19,11 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_create.html'
     fields = '__all__'
+    login_url = 'login'
 
 class PostUpdateView(UpdateView):
     model = Post
@@ -30,4 +35,7 @@ class PostDeleteView(DeleteView):
     template_name = 'post_delete.html'
     success_url = reverse_lazy("posts")
     
-
+class SignUp(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy("login")
