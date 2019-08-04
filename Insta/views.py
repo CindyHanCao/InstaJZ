0233a97d0ccc0e9a45cb5ae778c7206bcc97b622
naonaomacindy <chcaohan@gmail.com>
2019-08-04 08:22:08 -0700

    Second Commit

--- Insta/views.py
+++ Insta/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView
 
-# Create your views here.
+from Insta.models import Post
+
+class HelloWorld(TemplateView):
+    template_name = 'test.html'
+
+class PostsView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
