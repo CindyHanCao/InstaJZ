c86d3e08439ede097faf1439d6c171952ae49b18
naonaomacindy <chcaohan@gmail.com>
2019-08-04 10:22:30 -0700

    Update, Delete, New

--- Insta/views.py
+++ Insta/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from Insta.models import Post
 
@@ -12,3 +14,20 @@ class PostsView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = '__all__'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = ['title']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy("posts")
+    
+
--- InstaJZ/urls.py
+++ InstaJZ/urls.py
@@ -20,4 +20,5 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('insta/', include('Insta.urls')),
+    # path('', include('Insta.urls')),
 ]
